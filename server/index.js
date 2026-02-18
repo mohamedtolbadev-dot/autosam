a2e1fcc273026872f4f3f68f8c077475ce893d7f
coprino72 <coprino72@gmail.com>
2026-02-18 14:03:17 +0000

    Initial commits

--- server/index.js
+++ server/index.js
@@ -95,7 +95,7 @@ app.get('/', (req, res) => {
 });
 
 // Port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
 });
